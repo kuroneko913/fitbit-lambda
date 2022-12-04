49c897c414f3925cb2b06888d101dfcce0fdf0a7
kuroneko913 <kuroneko913@users.noreply.github.com>
2022-12-05 02:44:13 +0900

    [add] FitbitLambdaStack

---
+++ FitbitCDK/api_stack.py
@@ -0,0 +1,16 @@
+from .fitbit_api_caller import FitbitApiCaller
+from .fitbit_api import FitbitApi
+from .fitbit_notifier import FitbitNotifier
+from .fitbit_lowpower_notifier import FitbitLowpowerNotifier
+from constructs import Construct
+from aws_cdk import (
+    Stack
+)
+class ApiStack(Stack):
+    def __init__(self, scope:Construct, id:str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        FitbitApi(self, 'FitbitApi')
+        FitbitApiCaller(self, 'FitbitApiCaller')
+        FitbitNotifier(self, 'FitbitNotifier')
+        FitbitLowpowerNotifier(self, 'FitbitLowpowerNotifier')
